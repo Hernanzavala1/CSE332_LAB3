09fc00717a3aeb16544c5b6fd5ea7ab2a832a4d0
HernanZ <hernanjosuezavala@hotmail.com>
2020-10-24 00:32:38 -0400

    i think i finished all of the graphs and they are alright. Just need to do the rerport and record the presentation

--- app/charts.py
+++ app/charts.py
@@ -14,31 +14,24 @@ from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import euclidean_distances
 from sklearn.manifold import MDS
+from sklearn.metrics import pairwise_distances
 app = dash.Dash()
+import numpy as np
 with open("./original_police_Killings.csv", 'rb') as rawdata:
     result = chardet.detect(rawdata.read(100000))
 df = pd.read_csv("./original_police_Killings.csv", encoding="ISO-8859-1")
 df.head()
 app.layout = html.Div(className='layout', children=[
     html.Nav(className="Navbar", children=[
-    #     html.Ul(className="options", children=[
-    #     html.Button("Correlation", id="correlationBtn"),
-    #     html.Button("5×5 scatter plot matrix", id="scatter_plot_btn"),
-    #    html.Button("parallel coordinates", id="parrallel_btn"),
-    #    html.Button("PCA plot ", id="pca_btn"),
-    #     html.Button("biplot", id="biplot_btn"),
-    #     html.Button("MDS display(euclidian)", id="msd1_btn"),
-    #     html.Button("MDS display(euclidian)", id="msd2_btn")
-    #         ])
+
     dcc.Dropdown(id="options", options=[
             {'label': 'Correlation Matrix', 'value': 'correlation'},
             {'label': '5×5 scatter plot matrix', 'value': 'scatterplot'},
             {'label': 'parallel coordinates', 'value': 'parallel'},
             {'label': 'PCA plot', 'value': 'pca'},
             {'label': 'biplot', 'value': 'biplot'},
-            {'label': 'State', 'value': 'State'},
             {'label': 'MDS display(euclidian)', 'value': 'msd1'},
-            {'label': 'MDS display(euclidian)', 'value': 'msd2'},
+            {'label': 'MDS display(1-|correlation|)', 'value': 'msd2'},
            
         ],
             value='correlation',
@@ -70,22 +63,47 @@ def display_graph(options):
         return display_pca(),  {'display':'block'},  get_scree()
     elif options == 'biplot':
         return display_biplot(),  {'display':'none'},  dash.no_update
-    # elif options == 'msd1':
-    #     return display_mds_euclidian(),  {'display':'none'},  dash.no_update
+    elif options == 'msd1':
+        return display_mds_euclidian(),  {'display':'none'},  dash.no_update
+    elif options == 'msd2':
+        return display_mds_second(),  {'display':'none'},  dash.no_update
+def display_mds_second():
+    figure = None
+    df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
+    correlated_data = df2.corr(method='pearson').abs()
+    similarities = 1 - correlated_data
+    embedding = MDS(n_components=2, metric=True,  dissimilarity = "precomputed")
+    df_transformed = embedding.fit_transform(similarities)
+    print(df_transformed)
+    # mds_data = MDS(n_components=2, dissimilarity='precomputed')
+    # similarity = pairwise_distances(df2, metric='correlation')
+    # originalMDSCo = mds_data.fit_transform(similarity)
+    figure= px.scatter(df_transformed,x = 0, y = 1 , height= 500, color=1 ,color_continuous_scale= "RdBu", color_continuous_midpoint=1)
+    figure.update_layout(legend_title="Legend Title", title ="MDS PLOT 1 -|correlation|")
+    return figure
 def display_mds_euclidian():
     figure = None
     df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
-    euc = euclidean_distances(df2)
     embedding = MDS(n_components=2, metric=True,  dissimilarity = "euclidean")
     df_transformed = embedding.fit_transform(df2)
-    print(type(df_transformed))
+    # print(df_transformed)
+    figure= px.scatter(df_transformed, height=500, color= 1, x =0, y =1)
+    figure.update_layout(legend_title="Legend Title", title ="MDS PLOT EUCLEDIAN")
     return figure
 def display_biplot():
     df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
     x = StandardScaler().fit_transform(df2)
     pca = PCA(n_components=10)
     components = pca.fit_transform(x)
-    figure= px.scatter(components, x=0, y=1, color=df['raceethnicity'])
+    loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+    figure= px.scatter(components, x=0, y=1, height=500 ,  color=df['raceethnicity'], title="Biplot")
+    for i in range(10):
+        figure.add_shape(
+            type='line',
+            x0=0, y0=0,
+            x1=loadings[i, 0],
+            y1=loadings[i, 1]
+        )
     return figure
 def get_scree():
     df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
@@ -96,27 +114,24 @@ def get_scree():
     eigenVals = []
     for i in pca.explained_variance_:
         eigenVals.append(i)
-    y = pd.DataFrame(list(zip( eigenVals, indices,)), columns=[ "Eigen_Values","Index"])
-    return px.bar(y, x='Index', y = 'Eigen_Values')
+    y = pd.DataFrame(list(zip( eigenVals, indices,)), columns=[ "Eigen Values","Index"])
+    return px.bar(y, x='Index', y = 'Eigen Values', title ="Scree Plot")
 def display_pca():
     df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
-    df2.columns = ["Age", "Population","Poverty Rate","College rate", "Unemployment Rate", "County Income", "House hold income","White Population" , "Black Population" ,'Hispanic Population']
-    features = ["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]
+    
     x = StandardScaler().fit_transform(df2)
     pca = PCA(n_components=2)
     components = pca.fit_transform(x)
-    figure= px.scatter(components, x=0, y=1, color=df['raceethnicity'])
-    # df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
-    # df2.columns = ["Age", "Population","Poverty Rate","College rate", "Unemployment Rate", "County Income", "House hold income","White Population" , "Black Population" ,'Hispanic Population']
-    # x = StandardScaler().fit_transform(df2)
-    # pca = PCA(n_components=10)
-    # components = pca.fit_transform(x)
-    # indices = ['1', '2', '3', '4', '5', '6', '7',  '8',  '9',  '10']
-    # eigenVals = []
-    # for i in components.explained_variance:
-    #     eigenVals.append(i)
-    # dataF = pd.DataFrame(zip(eigenVals, indices), columns=["explained Variance", "eigen"])
-    # figure= px.bar(dataF)
+    loadings = pca.components_.T * np.sqrt(pca.explained_variance_)
+    figure= px.scatter(components,height=500,  x=0, y=1, color=df['raceethnicity'], title="PCA Plot")
+    for i in range(2):
+        figure.add_shape(
+            type='line',
+            x0=0, y0=0,
+            x1=loadings[i, 0],
+            y1=loadings[i, 1]
+        )
+        
     return figure
 def display_parallel():
     figure =None
@@ -124,15 +139,9 @@ def display_parallel():
     correlated_data = df2.corr(method='pearson').abs().sum(axis=0).sort_values(ascending=False, inplace=False)
     A1 = correlated_data[0]
     correlated_data= df2.corr(method='pearson')
-    # print(correlated_data)
-    # already_viewed = ['pov']
-    # for index in correlated_data.index:
-    #     if(index != 'pov'):
-    #         print(index)
-    #         print(correlated_data[index].argmax())
-    # print(already_viewed) 
     df3 = df2[["pov", "h_income", "college", "urate",'share_black', 'share_white',  'share_hispanic', 'age', 'pop', 'county_income']]
-    figure = px.parallel_coordinates(df3 ,color_continuous_midpoint=2)
+    figure = px.parallel_coordinates(df3,title ="Parallel Coordinates",  height=500 ,color_continuous_scale =None,range_color =[1,2], color_continuous_midpoint=2, 
+    labels={"pov":"Poverty Rate", "h_income":"House Hold Income", "college":"College Rate", "urate":"Unemployment Rate", "share_black":"Black Population", "share_white":"White Population", "share_hispanic":"Hispanic Population", "age":"Age", "pop":"Population", "county_income":"County Income"})
     return figure
 
 def display_scatterplot():
@@ -144,19 +153,19 @@ def display_scatterplot():
     for name, val in sum_values[0:5].iteritems():
         names.append(name)
     df3 = df2[names]
-    return px.scatter_matrix(df3)
+    return px.scatter_matrix(df3, height=800 , labels={"pov":"Poverty Rate", "h_income":"House Hold Income", "college":"College Rate", "urate":"Unemployment Rate", "share_black":"Black Population", "share_white":"White Population", "share_hispanic":"Hispanic Population", "age":"Age", "pop":"Population", "county_income":"County Income"})
 
 def display_correlation():
     df2 = df[["age","pop", "pov", "college", "urate", "county_income",  "h_income", "share_white", "share_black", "share_hispanic"]]
     correlated_data = df2.corr(method='pearson')
     # print(correlated_data)
-    figure = go.Figure(data=go.Heatmap(
+    figure = go.Figure( data=go.Heatmap(
                    z=[correlated_data["age"],correlated_data["pop"], correlated_data["pov"], correlated_data["college"], correlated_data["urate"], correlated_data["county_income"], correlated_data["h_income"], correlated_data["share_white"], correlated_data["share_black"], correlated_data["share_hispanic"]],
                 x=['Age', 'Population', 'Poverty Rate', 'College Rate', 'Unemployment Rate', 'County Income', 'House Hold Income', 'White Population', 'Black Population', 'Hispanic Population'], 
                    y=['Age', 'Population', 'Poverty Rate', 'College Rate', 'Unemployment Rate', 'County Income', 'House Hold Income', 'White Population', 'Black Population', 'Hispanic Population'],
             colorscale = 'RdBu', hoverongaps = False ))
 
-    figure.update_layout(
+    figure.update_layout(height = 500, 
     title='Correlation Matrix')
     return figure
 
